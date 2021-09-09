182ca3e87bbaa4ef725fc26444700237e8abdd49
Tri Bayu Priangga <87335431+TBPriangga@users.noreply.github.com>
2021-09-09 13:06:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+const inputNumber = (number) => {
+    if (currentNumber === '0') {
+        currentNumber = number
+    } else {
+    currentNumber += number
+    }
+}
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+const numbers = document.querySelectorAll(".number")
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+const operators = document.querySelectorAll(".operator")
+const inputOperator = (operator) => {
+    if (calculationOperator === '') {
+    prevNumber = currentNumber
+    }
+    calculationOperator = operator
+    currentNumber = '0'
+}
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) =>{
+        inputOperator(event.target.value)
+    })
+})
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click', () => {
+    calculate()
+    updateScreen(currentNumber)
+})
+const calculate = () => {
+    let result = ''
+    switch(calculationOperator) {
+        case "+":
+            result = parseFloat (prevNumber) + parseFloat (currentNumber)
+            break
+        case "-":
+            result = parseInt (prevNumber) - parseInt (currentNumber)
+            break
+        case "*":
+            result = parseInt (prevNumber) * parseInt (currentNumber)
+            break
+        case "/":
+            result = parseInt (prevNumber) / parseInt (currentNumber)
+            break
+        default:
+            return         
+    }
+    currentNumber = result
+    calculationOperator = ''
+}
+const clearALL = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
+}
+const clearBtn = document.querySelector('.all-clear')
+
+clearBtn.addEventListener('click', () => {
+    clearALL()
+    updateScreen(currentNumber)
+})
+inputDecimal = (dot) => {
+    if(currentNumber.includes('.')){
+        return
+    }
+    currentNumber += dot
+}
+const decimal = document.querySelector('decimal')
+
+decimal.addEventListener('click', (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
